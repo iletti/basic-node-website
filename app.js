9977242f2ebfd05a3d7eeaa113f120e51e5a07f4
iletti <ottoilarischmidt@gmail.com>
2022-12-26 12:45:25 +0200

    layouts activated

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
 const express = require('express');
 const app = express();
+var expressLayouts = require('express-ejs-layouts');
+
 const bodyParser = require('body-parser');
 
+//asettaa ejs view enginen
 app.set('view engine', 'ejs');
+
+//aktivoi express ejs layoutin (lisää routes/kansiossa ja layouts.ejs tiedostossa)
+app.use(expressLayouts);
+
+
 app.use(bodyParser.urlencoded({ extended: false }));
 
+
+//tähän alasivujen routes tiedostosijainnit 
 app.use('/', require('./routes/index'));
-app.use('/about', require('./routes/about'));
 
 
+//laittaa serverin kuuntelemaan porttia x, käynnistä $ node app.js komennolla
 app.listen(3000, () => {
   console.log('Server listening on port 3000');
 });
